1434864b6bfb187ec35466af9b783d25757eb455
Huy-DNA <huydo862003@gmail.com>
2026-05-07 20:11:53 +0700

    test: add some expression formatting tests for eslint

---
+++ packages/nuclint/__tests__/fixtures/array-bracket-newline/valid/multiline.ts
@@ -0,0 +1,8 @@
+const _a = [
+  1,
+  2,
+];
+
+const _b = [1];
+
+const _c: number[] = [];
---
+++ packages/nuclint/__tests__/fixtures/object-curly-newline/valid/multiline.ts
@@ -0,0 +1,9 @@
+declare const _a: {
+  abc: string;
+};
+
+const _b = {
+  abc: '123',
+};
+
+const _c = {};
