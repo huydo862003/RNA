6fc6cf9a46f10c0bc06c9043d52565beb1151fae
Huy-DNA <huydo862003@gmail.com>
2026-05-08 17:55:23 +0700

    feat(fck-AI-slop): add root eslint

---
+++ eslint.config.ts
@@ -0,0 +1,13 @@
+/* #human-slop
+   https://github.com/huydo862003/Fck-AI-Slop/edit/main/README.md
+*/
+
+import {
+  vueConfig,
+  baseConfig,
+} from '@hdnax/nuclint';
+
+export default [
+  ...vueConfig,
+  ...baseConfig,
+];
